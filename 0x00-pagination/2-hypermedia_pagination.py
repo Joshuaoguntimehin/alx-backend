de58bf7fd2d04e9d3ac9f0d26cbd8bc42b7dcb2c
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-29 01:03:36 +0100

    alx tasks

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""import statement"""
+import math
+import csv
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # Skip header row
+
+        return self.__dataset
+    
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a page of the dataset."""
+        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
+
+        start_index = (page - 1) * page_size
+        end_index = page * page_size
+
+        dataset = self.dataset()
+        return dataset[start_index:end_index]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict[str, any]:
+        """Returns a dictionary with pagination details."""
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+        }
