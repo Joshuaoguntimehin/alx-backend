87a72b7367764d26b88a0129c280de31998de742
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-29 01:08:02 +0100

    alx tasks

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -22,18 +22,18 @@ class Server:
             self.__dataset = dataset[1:]  # Skip header row
 
         return self.__dataset
-    
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Returns a page of the dataset."""
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
+        assert isinstance(page, int) and page > 0, "Page integer."
+        assert isinstance(page_size, int) and page_size > 0, "Page ."
 
         start_index = (page - 1) * page_size
         end_index = page * page_size
 
         dataset = self.dataset()
         return dataset[start_index:end_index]
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict[str, any]:
         """Returns a dictionary with pagination details."""
         data = self.get_page(page, page_size)
