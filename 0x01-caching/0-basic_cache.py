7390b50b3a0c6db5dfd05381317c67720a57e6d0
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-31 00:38:01 +0100

    alx tasks

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""IMPORT statement"""
+""" BasicCache module """
+
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    def __init__(self):
-        super().__init__()
+    """ BasicCache is a caching system that has no limit """
 
     def put(self, key, item):
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Return the value linked to key."""
-        if key is None:
-            return None
-        return self.cache_data.get((key))
+        """ Get an item by key from the cache """
+        return self.cache_data.get(key) if key is not None else None
