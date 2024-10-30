661a675d5bd8b7b7bbe27a705335478a5ea94811
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-31 00:45:32 +0100

    alx tasks

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""import statement"""
+""" LIFOCache module """
+
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
+    """ LIFOCache is a caching system that follows the LIFO algorithm """
+
     def __init__(self):
+        """ Initialize the cache """
         super().__init__()
+        self.last_key = None  # To track the last added key
 
     def put(self, key, item):
-        if key is None or item is None:
-            return
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = next(reversed(self.cache_data))
-            print(f'DISCARD: {last_key}')
-            del self.cache_data[last_key]
+        """ Add an item to the cache following LIFO policy """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            self.last_key = key  # Update the last added key
 
-        self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # LIFO: Discard the last inserted key
+                del self.cache_data[self.last_key]
+                print(f"DISCARD: {self.last_key}")
 
     def get(self, key):
-        if key is None:
-            return None
-        return self.cache_data.get((key))
+        """ Get an item by key from the cache """
+        return self.cache_data.get(key) if key is not None else None
