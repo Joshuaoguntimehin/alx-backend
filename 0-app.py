fcfc8a84cc09f7245ec9860fd5a855a18b01edd0
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-05 16:01:47 +0100

    alx tasks

---
+++ 0-app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template 
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+@app.route("/")
+def nothing():
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ 1-app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template,request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+app.config['DEFAULT_TIMEZONE'] = 'UTC'
+app.config['BABEL_DEFAULT_LOCATE'] = "en"
+app.config['BABEL_SUPPORTED_LOCALES'] = ["en", "fr"]
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
